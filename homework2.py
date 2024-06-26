a2f459e7ec80b1ce826246bc9bf3b0a6e1e8aa41
Maksim2001-web <89614701484@mail.ru>
2024-06-26 20:08:42 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+completed_homeworks = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_per_homework = hours_spent / completed_homeworks
+print(f"Курс: {course_name}, всего задач: {completed_homeworks}, затрачено часов: {hours_spent}, среднее время выполнения {time_per_homework:.3f} часа.")
